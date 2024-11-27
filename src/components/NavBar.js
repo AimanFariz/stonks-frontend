1e0f57f4d82d62fdf77599ecbf7a3424bf5d6e3a
Aiman Fariz <99475959+AimanFariz@users.noreply.github.com>
2024-11-27 10:55:47 -0600

    nothing much. check logs for more info duhh

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar() {
   return (
     <div className='fixed justify-around flex w-full bg-gray-800 text-white p-2 text-center items-center'>
-        <p>Meme Genie</p>
+        <p>Stonks</p>
         <a href='https://drive.google.com/drive/folders/1HA9w4OwQvbPb_-RgcfNRwp0WbpzbSah0?usp=drive_link' className='border border-slate-400 p-3 bg-cyan-500 rounded-md text-white font-medium hover:bg-cyan-400'>Donate Memes here</a>
     </div>
   )
